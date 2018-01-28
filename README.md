2e169f3a8531a4bfa799445154bce83038b09a3c
Frank Mazzarella <mazzarella.frank@gmail.com>
2018-01-27 19:15:49 -0500

    basic webpack configuration

---
+++ README.md
@@ -0,0 +1,1 @@
+Development portfolio for Frank Mazzarella
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
-var webpack = require('webpack');
-var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+var ExtractTextPluginConfig = new ExtractTextPlugin('style.css');
 
 module.exports = {
-  debug: true,
-  entry: {
-    main: './src/app.js'
-  },
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js'
   },
   module: {
-    loaders: []
-  }
-};
+    loaders: [
+      {
+        test: /\.js$/,
+        include: __dirname + '/src',
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      },
+      {
+        test: /\.scss$/,
+        include: __dirname + '/src',
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig, ExtractTextPluginConfig]
+}

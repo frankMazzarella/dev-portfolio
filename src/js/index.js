19d84dea05c20d191cda94eec7e67424b82a191d
Frank Mazzarella <mazzarella.frank@gmail.com>
2018-02-05 23:19:48 -0500

    adding how many years js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
 import '../scss/index.scss';
 
 // TODO: there is an issue with babel. it wants to replace es2015 with 'env'. do something
+
+const howManyYearsEl = document.getElementById('how-many-years');
+const startDate = Date.parse('Feb 1, 2016');
+const now = new Date().getTime();
+const oneYear = 1000 * 60 * 60 * 24 * 365;
+const years = (now - startDate) / oneYear;
+howManyYearsEl.textContent = years.toFixed(1);

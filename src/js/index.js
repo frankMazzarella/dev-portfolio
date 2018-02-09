ae55659d657ef140fbf8dfb34c2cb70b1a0340a3
Frank Mazzarella <mazzarella.frank@gmail.com>
2018-02-09 00:25:48 -0500

    adding the jquerys

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,47 @@ const now = new Date().getTime();
 const oneYear = 1000 * 60 * 60 * 24 * 365;
 const years = (now - startDate) / oneYear;
 howManyYearsEl.textContent = years.toFixed(1);
+
+$(document).ready(() => {
+  /* Animations on scroll */
+  $('.js--wp-1').waypoint((direction) => {
+    $('.js--wp-1').addClass('animated fadeInUp');
+  }, {
+    offset: '50%',
+  });
+
+  $('.js--wp-2').waypoint((direction) => {
+    $('.js--wp-2').addClass('animated fadeIn');
+  }, {
+    offset: '50%',
+  });
+
+  $('.js--wp-3').waypoint((direction) => {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+    offset: '50%',
+  });
+
+  $('.js--wp-4').waypoint((direction) => {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%',
+  });
+
+      /* Navigation scroll */
+      $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+});
+

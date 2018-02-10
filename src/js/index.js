c5ecacba8a67edc01baf05977010d1a5b77dc56a
Frank Mazzarella <mazzarella.frank@gmail.com>
2018-02-09 21:49:55 -0500

    fancy typing animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,36 @@
 import $ from 'jquery';
 import ScrollReveal from 'scrollreveal';
+import Typed from 'typed.js';
 import '../scss/index.scss';
 
 // TODO: there is an issue with babel. it wants to replace es2015 with 'env'. do something
-// TODO: use this https://mattboldt.com/demos/typed-js/
+// TODO: is the animate css lib needed?
+
+const $howManyYears = $('#how-many-years');
 
 $(document).ready(() => {
+  animatedTyping();
   calculateHowManyYears();
   addAnimations();
 });
 
 function addAnimations() {
   const sr = ScrollReveal();
-  sr.reveal('.fade-in');
+  sr.reveal('.fade-in'); // TODO: fine tune me
 }
 
 function calculateHowManyYears() {
-  const howManyYearsEl = document.getElementById('how-many-years');
   const startDate = Date.parse('Feb 1, 2016');
   const now = new Date().getTime();
   const oneYear = 1000 * 60 * 60 * 24 * 365;
   const years = (now - startDate) / oneYear;
-  howManyYearsEl.textContent = years.toFixed(1);
+  $howManyYears.text(years.toFixed(1));
 }
 
+function animatedTyping() {
+  const typed = new Typed('#typed-out', {
+    stringsElement: '#header-typed',
+    typeSpeed: 40,
+    startDelay: 1000,
+  });
+}

6ad0650079b1d6941ffd8ede8a0dc3117a38459d
Frank Mazzarella <mazzarella.frank@gmail.com>
2018-02-11 02:23:26 -0500

    smooth scrolling

--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,17 @@ $(document).ready(() => {
   calculateHowManyYears();
   addAnimations();
   slickCarousel();
+  smoothScrolling();
 });
+
+function smoothScrolling() {
+  $(document).on('click', 'a[href^="#"]', function smoothScroll(event) {
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top,
+    }, 500);
+  });
+}
 
 function addAnimations() {
   const sr = ScrollReveal();
